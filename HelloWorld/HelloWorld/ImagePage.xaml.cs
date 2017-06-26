9a473f5565472585400a9493960c991267a8c334
Gary Parkin <gary.parkin@conduent.com~>
2017-06-26 10:49:06 -0400

    How to download an app with a progress indicator.

--- HelloWorld/HelloWorld/ImagePage.xaml.cs
+++ HelloWorld/HelloWorld/ImagePage.xaml.cs
@@ -18,13 +18,8 @@ namespace HelloWorld
 
          // If you do this in xaml, the image will be cashed for 24 hours, so we do it here
          UriImageSource imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/sports/7/") };
-
          // This removes casching
          imageSource.CachingEnabled = false;
-
-         // We could set the length of casching to 1 hour
-         //imageSource.CacheValidity = TimeSpan.FromHours(1);
-
          image.Source = imageSource;
 		}
 	}
