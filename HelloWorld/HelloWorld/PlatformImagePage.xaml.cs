284e7ca572974da3aae6e14d47be73c62e2eb0a1
Gary Parkin <gary.parkin@conduent.com~>
2017-06-29 12:39:25 -0400

    Differenrt images per platform

--- HelloWorld/HelloWorld/PlatformImagePage.xaml.cs
+++ HelloWorld/HelloWorld/PlatformImagePage.xaml.cs
@@ -9,15 +9,36 @@ using Xamarin.Forms.Xaml;
 
 namespace HelloWorld
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PlatformImagePage : ContentPage
-	{
-		public PlatformImagePage ()
-		{
-			InitializeComponent ();
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+   public partial class PlatformImagePage : ContentPage
+   {
+      public PlatformImagePage()
+      {
+         InitializeComponent();
 
-         btn.Image = (FileImageSource) ImageSource.FromFile("clock.png");
+         // btn.Image = (FileImageSource) ImageSource.FromFile("clock.png");
 
-		}
-	}
+         // Device.OnPlatform is obsolete
+         //btn.Image = (FileImageSource)ImageSource.FromFile(Device.OnPlatform(
+         //   iOS: "clock.png",
+         //   Android: "clock.png",
+         //   WinPhone: "images/clock.png"
+         //   ));
+
+         switch (Device.RuntimePlatform)
+         {
+            case Device.iOS:
+               btn.Image = (FileImageSource)ImageSource.FromFile("clock.png");
+               break;
+            case Device.Android:
+               btn.Image = (FileImageSource)ImageSource.FromFile("clock.png");
+               break;
+            case Device.Windows:
+               btn.Image = (FileImageSource)ImageSource.FromFile("images/clock.png");
+               break;
+         }
+
+
+      }
+   }
 }
