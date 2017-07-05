9cb18150aa8e4e8bfda55a6fabf7fc537dc49e28
Gary Parkin <gary.parkin@conduent.com~>
2017-07-05 09:30:32 -0400

    ListView with a class.  Simple Image, Text and a Detail. WORKS!

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -14,7 +14,7 @@ namespace HelloWorld
 		{
 			InitializeComponent();
 
-         MainPage = new HelloWorld.PlatformImagePage();
+         MainPage = new HelloWorld.ListDemo();
 		}
 
 		protected override void OnStart ()
---
+++ HelloWorld/HelloWorld/ListDemo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+   [XamlCompilation(XamlCompilationOptions.Compile)]
+   public partial class ListDemo : ContentPage
+   {
+      public ListDemo()
+      {
+         InitializeComponent();
+
+         List<string> names = new List<string>
+         {
+            "Mosh",
+            "John",
+            "Bob"
+         };
+
+         listView.ItemsSource = new List<Contact>
+         {
+            new Contact {Name = "Mosh", ImageUrl="http://lorempixel.com/100/100/people/1"},
+            new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="hey, let's talk"}
+         };
+
+
+
+
+      }
+   }
+}
