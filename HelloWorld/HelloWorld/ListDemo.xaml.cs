6f1e30995c03f4817f0b44b773972a2c1ee418e3
Gary Parkin <gary.parkin@conduent.com~>
2017-07-06 12:18:19 -0400

    Grouping Contacts

--- HelloWorld/HelloWorld/ListDemo.xaml.cs
+++ HelloWorld/HelloWorld/ListDemo.xaml.cs
@@ -24,10 +24,16 @@ namespace HelloWorld
             "Bob"
          };
 
-         listView.ItemsSource = new List<Contact>
+         listView.ItemsSource = new List<ContactGroup>
          {
+            new ContactGroup("M", "M")
+            {
             new Contact {Name = "Mosh", ImageUrl="http://lorempixel.com/100/100/people/1"},
-            new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="hey, let's talk"}
+            },
+            new ContactGroup("J", "J")
+            {
+            new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="Hey, let's talk"}
+            }
          };
 
 
