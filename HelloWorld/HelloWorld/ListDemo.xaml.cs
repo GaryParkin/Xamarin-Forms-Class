ef3e0c75222a003045eb875575be54ebbe6f946b
Gary Parkin <gary.parkin@conduent.com~>
2017-07-07 09:57:09 -0400

    Added Tapped and Selected Events

--- HelloWorld/HelloWorld/ListDemo.xaml.cs
+++ HelloWorld/HelloWorld/ListDemo.xaml.cs
@@ -30,14 +30,29 @@ namespace HelloWorld
             new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="hey, let's talk"}
          };
 
+      }
 
+      // IOS works differently then Android.  
+      // In IOS the first time you click the row, you get the Selected event.  The next time you only get the Tapped event.
+      // In Android, the first time you click the row, you get both events. Tapped, then Selected, and it makes no difference what order they are.
+      // And the next time you only get the Tapped event.
 
+      private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+      {
+         Contact contact = e.Item as Contact;
+         DisplayAlert("Tapped", contact.Name, "OK");
 
       }
 
       private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
       {
+         Contact contact = e.SelectedItem as Contact;
+         DisplayAlert("Selected", contact.Name, "OK");
 
+         // This would disable the selection - The above code also needs to be commented out if you use this
+         // You'd use this to tap the row and keep it from being selected.
+         // listView.SelectedItem = null;
       }
+
    }
 }
