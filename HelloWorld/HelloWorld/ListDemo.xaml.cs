f4183af2c2a3dd6ec576a283eff7ffa2edeb00b3
Gary Parkin <gary.parkin@conduent.com~>
2017-07-14 09:18:32 -0400

    Added pulldown to refresh the list

--- HelloWorld/HelloWorld/ListDemo.xaml.cs
+++ HelloWorld/HelloWorld/ListDemo.xaml.cs
@@ -13,26 +13,28 @@ namespace HelloWorld
    [XamlCompilation(XamlCompilationOptions.Compile)]
    public partial class ListDemo : ContentPage
    {
+      List<Contact> GetContacts()
+      {
+         return new List<Contact> {
+                     new Contact {Name = "Mosh", ImageUrl="http://lorempixel.com/100/100/people/1"},
+                     new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="hey, let's talk"}
+                  };
+      }
+
       public ListDemo()
       {
          InitializeComponent();
 
-         List<string> names = new List<string>
-         {
-            "Mosh",
-            "John",
-            "Bob"
-         };
 
-         listView.ItemsSource = new List<Contact>
-         {
-            new Contact {Name = "Mosh", ImageUrl="http://lorempixel.com/100/100/people/1"},
-            new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="hey, let's talk"}
-         };
-
-
-
-
+         listView.ItemsSource = GetContacts();
       }
+
+      private void listView_Refreshing(object sender, EventArgs e)
+      {
+         listView.ItemsSource = GetContacts();
+
+         listView.EndRefresh();
+      }
+
    }
 }
