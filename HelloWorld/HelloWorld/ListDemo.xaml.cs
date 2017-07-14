3f5678dae027e8144590a56735489796785b723d
Gary Parkin <gary.parkin@conduent.com~>
2017-07-14 09:58:35 -0400

    Added a search - truly easy.

--- HelloWorld/HelloWorld/ListDemo.xaml.cs
+++ HelloWorld/HelloWorld/ListDemo.xaml.cs
@@ -13,20 +13,31 @@ namespace HelloWorld
    [XamlCompilation(XamlCompilationOptions.Compile)]
    public partial class ListDemo : ContentPage
    {
+      IEnumerable<Contact> GetContacts(string searchText = null)
+      {
+         IEnumerable<Contact> contacts = new List<Contact> {
+                     new Contact {Name = "Mosh", ImageUrl="http://lorempixel.com/100/100/people/1"},
+                     new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="hey, let's talk"},
+                     new Contact {Name = "Jim", ImageUrl="http://lorempixel.com/100/100/people/3"},
+                     new Contact {Name = "Joey", ImageUrl="http://lorempixel.com/100/100/people/4", Status="Yo, Joey!"}
+                  };
+
+         if (String.IsNullOrWhiteSpace(searchText))
+            return contacts;
+
+         return contacts.Where(c => c.Name.StartsWith(searchText, StringComparison.OrdinalIgnoreCase));
+      }
+
       public ListDemo()
       {
          InitializeComponent();
 
+         listView.ItemsSource = GetContacts();
+      }
 
-         listView.ItemsSource = new List<Contact>
-         {
-            new Contact {Name = "Mosh", ImageUrl="http://lorempixel.com/100/100/people/1"},
-            new Contact {Name = "John", ImageUrl="http://lorempixel.com/100/100/people/2", Status="hey, let's talk"}
-         };
-
-
-
-
+      private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+      {
+         listView.ItemsSource = GetContacts(e.NewTextValue);
       }
    }
 }
