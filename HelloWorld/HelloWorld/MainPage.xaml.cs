4d451608be8d42cf035373d8056751541a0df3e1
Gary Parkin <gary.parkin@conduent.com~>
2017-07-19 13:44:36 -0400

    Added some popups, display alert and an action sheet.

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -1,17 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace HelloWorld
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+   public partial class MainPage : ContentPage
+   {
+      public MainPage()
+      {
+         InitializeComponent();
+      }
+
+      private async void Button_Clicked(object sender, EventArgs e)
+      {
+         var response = await DisplayAlert("Warning", "Are you sure?", "Yes", "No");
+         if (response)
+            await DisplayAlert("Done", "Your response will be saved", "OK");
+      }
+
+      private async void Button_Clicked_1(object sender, EventArgs e)
+      {
+         var response = await DisplayActionSheet("title", "Cancel", "Delete", "Copy Link", "Message", "email");
+         await DisplayAlert("Response", response, "OK");
+
+      }
+   }
 }
