1fb70cc6adaaff383a182664b533091a3c1405eb
Gary Parkin <gary.parkin@conduent.com~>
2017-07-25 11:33:07 -0400

    Tested a menu idea but I can't figure out how to add a button to close the menu.  I don't think it's intended as there is room to click off the menu.

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -14,7 +14,7 @@ namespace HelloWorld
 		{
 			InitializeComponent();
 
-         MainPage = new HelloWorld.ListDemo();
+         MainPage = new MenuPage();
 		}
 
 		protected override void OnStart ()
